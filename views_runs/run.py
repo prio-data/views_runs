3196bdff77a0f8c2836520d4106c4273ceedb978
peder2911 <pglandsverk@gmail.com>
2021-10-22 17:00:45 +0200

    Added an example notebook

--- views_runs/run.py
+++ views_runs/run.py
@@ -11,20 +11,35 @@ class ViewsRun():
 
     This class combines modelling and partitioning, delivering predictions in a
     familiar format and concise API.
+
+    Takes two arguments, a views_partitioning.DataPartitioner instance, and a
+    stepshift.StepshiftedModels instance. These instances determine how data is
+    subset, and how modelling happens.
     """
 
     def __init__(self, partitioner: DataPartitioner, models: StepshiftedModels):
         self._models = models
-        self._partitioner = partitioner.shift_left(self._models._steps_extent)
+        self._shifted_partitioner = partitioner.shift_left(self._models._steps_extent)
+        self._partitioner = partitioner
 
     @validation.views_validate
     def fit(self, partition_name: str, timespan_name: str, data: pd.DataFrame)-> None:
-        self._models.fit(self._partitioner(partition_name,timespan_name,data))
+        """
+        Fit the model to the named partition. partition_name must be present in
+        the DataPartitioner.
+        """
+
+        self._models.fit(self._shifted_partitioner(partition_name,timespan_name,data))
 
     @validation.views_validate
     def predict(self, partition_name: str, timespan_name: str, data: pd.DataFrame)-> pd.DataFrame:
+        """
+        Predict with data from the named partition. partition_name must be
+        present in the DataPartitioner.
+        """
+
         predictions = self._models.predict(
-                self._partitioner(partition_name, timespan_name, data),
+                self._shifted_partitioner(partition_name, timespan_name, data),
                 combine = False)
         predictions.index.names = data.index.names
         data = data.merge(predictions, how = "left", left_index = True, right_index = True)
